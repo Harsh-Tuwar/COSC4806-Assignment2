ec37e839d368afde8c973bf6b3f6b872cba8c1ea
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2025-05-22 17:53:48 +0000

    added config with environment variables

---
+++ config.php
@@ -0,0 +1,10 @@
+
+<?php
+
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', $_ENV['DB_DATABASE']);
+define('DB_PORT',  $_ENV['DB_PORT']);
+
+?>
