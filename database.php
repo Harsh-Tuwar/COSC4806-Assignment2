d2b39784493a08324d91b6d9e966a1d180eda00c
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2025-05-22 18:00:06 +0000

    completed database.php

--- database.php
+++ database.php
@@ -2,6 +2,14 @@
 
 require_once 'config.php';
 
-
+function db_connect() {
+  try {
+    $conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_DATABASE . ';port=' . DB_PORT, DB_USER, DB_PASS);
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+  } catch (PODOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+  }
+}
 
 ?>
